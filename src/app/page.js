b7c17f3157a3b7fcf0d94af2af367182291e3f44
Iván Berruet <ivanberruet95@gmail.com>
2024-10-26 22:39:11 -0300

    test1

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,22 @@
+async function callHello(){
+  try {
+      let res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/hello`)
+      const contentType = res.headers.get("content-type");
+      const data = contentType && contentType.includes("application/json")
+      ? await res.json()
+      : await res.text(); // Handle text responselet data = await res.json() 
+      
+      return data
+  } catch (error) {
+      console.log(error)
+    }
+}
 
-export default function Home() {
+export default async function Home() {
+
+  const hello = await callHello();
 
   return (
-    <div>Hello</div>
+    <div>Mensaje de la API {hello}</div>
   );
 }
